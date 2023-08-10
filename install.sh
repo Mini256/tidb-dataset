9be082cecbfc77113e9166755cdb64d9ac2d8696
Mini256 <minianter@foxmail.com>
2023-08-10 11:32:27 +0800

    build: bump tiup demo to v0.0.11

--- install.sh
+++ install.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-version=0.0.10
+version=0.0.11
 
 case $(uname -s) in
     Linux|linux) os=linux ;;
--- publish.sh
+++ publish.sh
@@ -2,7 +2,7 @@
 set -e
 
 repo_name="Mini256/tidb-dataset"
-version="0.0.10"
+version="0.0.11"
 binary_name="tidb-dataset"
 component_name="demo"
 component_desc="The dataset import tools for the demo of TiDB"
